24725ecf1de22fd44aa8ad6558cb82d42bb19107
Ieva <ieva.bauer@gmail.com>
2016-10-04 17:15:25 +0300

    1 pamoka

---
+++ Nr.1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nr._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laba diena");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
